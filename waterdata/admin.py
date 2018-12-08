75a3ca6ba9fa239eaaf2f27f8adf40320b13f56f
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-12-09 00:58:52 +0500

    ran migrate on models

--- waterdata/admin.py
+++ waterdata/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ontology, Author
 
-# Register your models here.
+class OntologyAdmin(admin.ModelAdmin):
+     filter_horizontal = ('authors',) #If you don't specify this, you will get a multiple select widget.
+
+admin.site.register(Ontology,OntologyAdmin)
+admin.site.register(Author)
--- waterdata/models.py
+++ waterdata/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Ontology(models.Model):
+    name = models.CharField(max_length=40,null=False,blank=False) 
+    uri = models.URLField(max_length=200,null=False)
+    description = models.TextField(max_length=1000)
+    file = models.FileField(upload_to='uploads/ontologies/',blank=True)
+    license = models.CharField(max_length=100,blank=True)
+    authors = models.ManyToManyField('Author',blank=True)
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+	name = models.CharField(max_length=20) 
+	introduction = models.TextField(max_length=1000)
+	def __str__(self):
+		return self.name
+
+
