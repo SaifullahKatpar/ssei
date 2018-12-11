d77f8c6d7736f15f1cc2c8399a3aba25eb2f22af
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-12-11 15:08:20 +0500

    added owlwrapper python module

--- waterdata/views.py
+++ waterdata/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .forms import QueryForm
 from .models import Ontology
+from .owlreadywrapper import OWLReady
 
 
 def home(request):
@@ -18,3 +20,7 @@ def ontology_detail(request,ontology_id):
 	ontology = get_object_or_404(Ontology,pk=ontology_id)
 	return render(request,'waterdata/ontology_detail.html',{'ontology':ontology})
 
+def test(request):
+    obj = OWLReady()
+    res = obj.list_individuals()
+    return HttpResponse(res)
