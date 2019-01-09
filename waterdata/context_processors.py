a5e80ab03fedfb13f20abff9df6d4ef66ce00eca
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2019-01-09 14:37:56 +0500

    tested github ontology loading fail

--- waterdata/context_processors.py
+++ waterdata/context_processors.py
@@ -3,6 +3,7 @@ from owlready2 import *
 def water_classes(request):
     # get ontology and load classes for search autocomplete
     onto = get_ontology("uploads/ontologies/water.owl").load()
+    #onto = get_ontology("https://raw.githubusercontent.com/SaifullahKatpar/WaterOnto/master/water.owl").load()
     # string representaton of ThingClass types
     classes_list = [ (str(c).split('.')[1]).replace('_',' ') for c in onto.classes() ]
     return {'water_classes':classes_list}
