c8c6f76fac5fc949898ffd86ed48d913aac1d9fc
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2019-01-11 01:11:12 +0500

    added some nltk code for tokenization

--- waterdata/query_processor.py
+++ waterdata/query_processor.py
@@ -1,3 +1,15 @@
 
 
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
 
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+
+def preprocess(sent):
+    sent = word_tokenize(sent)
+    sent = pos_tag(sent)
+    return sent
