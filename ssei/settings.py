8b78f9d7f7e9d7729f49f45502a3a0ef1f08a3b9
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2019-02-07 15:36:43 +0500

    added dj-database-url package

--- ssei/settings.py
+++ ssei/settings.py
@@ -92,8 +92,6 @@ WSGI_APPLICATION = 'ssei.wsgi.application'
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
- """
-
 DATABASES = {
 
      'default': {
@@ -107,7 +105,8 @@ DATABASES = {
  
 
 }
-
+"""
+DATABASES = {default: dj_database_url.config()}
 
 '''     'default': {
         'ENGINE': 'django.db.backends.postgresql',
